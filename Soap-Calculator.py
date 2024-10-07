f82f466b11cb52ca0c81176d02a5c0cc8033b15d
KillianBlain <killian.blain@hotmail.fr>
2024-10-07 19:21:31 +0200

    Update Soap-Calculator.py
    Addition of a wide range of oil options for soapmaking.

--- Soap-Calculator.py
+++ Soap-Calculator.py
@@ -1,21 +1,13 @@
 import matplotlib.pyplot as plt
 
-#variables globales
-IndiceCoco=183
-IndiceKarite=128
-IndiceOlive=135
-IndiceAmande=137
-QSoudeCoco=0
-QSoudeKarite=0
-QSoudeOlive=0
-QSoudeAmande=0
-MasseCoco=0
-MasseKarite=0
-MasseOlive=0
-MasseAmande=0
+liste_saponification_naoh = [["abricot", 139], ["amande douce", 139], ["arachide", 137], ["argan", 136], ["avocat", 133], ["babassu", 175], ["boabab", 143], ["bourraches", 135], ["café", 128], ["camélia", 139], ["camelina", 134], ["canneberge", 135], ["canola", 133], ["carthame", 137], ["cassis", 133], ["cerises", 135], ["chanvre", 138], ["chardon marie", 126], ["citrouille", 139], ["colza", 124], ["coco", 183], ["coprah", 232], ["coton", 138], ["crisco", 137], ["cumin noir", 135], ["emu", 135], ["framboise", 134], ["fruit de la passion", 130], ["germe du blé", 130], ["jojoba ", 66], ["karanja", 131], ["kukui", 135], ["lin", 135], ["macadamia", 139], ["maïs", 137], ["mangues", 128], ["monoi de tahiti", 182], ["neem", 139], ["noisette", 139], ["noix", 135], ["oeillettes", 138], ["olive", 135], ["onagre", 136], ["palme", 142], ["palmiste", 176], ["pêches", 136], ["perilla", 135], ["pistache", 133], ["raisin", 129], ["ricin", 128], ["rose musquée", 133], ["sésame", 134], ["soja", 136], ["soja hydrogéné", 136], ["son de riz", 128], ["tamanu", 148], ["tournesol", 135], ["visons", 140]]
 
-#fonctions
-def calculeQuantiteSoude (MasseHuile, IndiceSapo): 
+def TypeHuile (): #permet d'afficher les choix d'huiles à l'utilisateur
+    print("Les choix d'huiles disponibles sont :")
+    for type_huiles in liste_saponification_naoh:
+        print("-",type_huiles[0],";")
+        
+def CalculQuantiteSoude (MasseHuile, IndiceSapo):
     QSoude=MasseHuile*IndiceSapo/1000
     return QSoude
 
@@ -32,50 +24,37 @@ def AfficheTypeSavon (Taux):
         print("Le savon est très surgras et très mou.")
     else: print("Le savon est trop riche en huile et risque de déphaser lors de la fabrication.")
 
-def TauxSurgraissage (MasseSoude, MasseCoco, MasseKarite, MasseOlive, MasseAmande):
-    MasseHuileTotale=MasseCoco+MasseAmande+MasseKarite+MasseOlive
-    PCoco=MasseCoco/MasseHuileTotale
-    PKarite=MasseKarite/MasseHuileTotale
-    POlive=MasseOlive/MasseHuileTotale
-    PAmande=MasseAmande/MasseHuileTotale
-
-    IndiceSapoMelange=(PCoco*IndiceCoco+PKarite*IndiceKarite+POlive*IndiceOlive+PAmande*IndiceAmande)
-
+def TauxSurgraissage (MasseSoude, MasseHuileTotale, IndiceSapoMelange):
     TauxPredit=((MasseHuileTotale-(MasseSoude/IndiceSapoMelange*1000))/MasseHuileTotale)*100
-
     return TauxPredit
 
-continuer=True #Demande des types d'huiles et des masses à utiliser pour la saponification
-while continuer :
-    TypeHuile=str(input("Préciser le type d'huile (coco, karité, olive ou amande douce):"))
-    while ((TypeHuile != "coco") and (TypeHuile != "karité") and (TypeHuile != "olive") and (TypeHuile != "amande douce")):
-        print("Seuls les choix suivants sont autorisés : coco, karité, olive ou amande douce")
-        TypeHuile=str(input("Préciser le type d'huile (coco, karité, olive ou amande douce):")) 
+TypeHuile() #Présente les choix d'huiles disponibles a l'utilisateur
 
-    MasseHuile=float(input("Entrer la quantité d'huile (en g):"))
-    while MasseHuile<0:
-        print("La quantité d'huile ne peut pas être négative")
+continuer=True
+soude_totale_theorique=0
+masse_totale_huiles=0
+indice_sapo_melange=0
+while continuer : #Demande les types d'huiles et les masses à utiliser pour la saponification
+    huiles = [huile[0] for huile in liste_saponification_naoh]
+    indices_saponification = [huile[1] for huile in liste_saponification_naoh]
+    TypeHuile=str(input("Préciser le type d'huile que vous souhaitez ajouter :"))
+    
+    if TypeHuile in huiles:
         MasseHuile=float(input("Entrer la quantité d'huile (en g):"))
-
-    if TypeHuile == "coco":
-        MasseCoco=MasseHuile
-        QSoudeCoco=calculeQuantiteSoude (MasseCoco,IndiceCoco)
-    elif TypeHuile == "karité":
-        MasseKarite=MasseHuile
-        QSoudeKarite=calculeQuantiteSoude (MasseKarite,IndiceKarite)
-    elif TypeHuile == "olive":
-        MasseOlive=MasseHuile
-        QSoudeOlive=calculeQuantiteSoude (MasseOlive,IndiceOlive)
-    elif TypeHuile == "amande douce":
-        MasseAmande=MasseHuile
-        QSoudeAmande=calculeQuantiteSoude (MasseAmande,IndiceAmande)
+        index = huiles.index(TypeHuile)
+        IndiceSapo = indices_saponification[index]
+        masse_soude_theorique = CalculQuantiteSoude(MasseHuile,IndiceSapo)
+        soude_totale_theorique += masse_soude_theorique
+        masse_totale_huiles += MasseHuile
+        indice_sapo_melange += IndiceSapo*MasseHuile
+    else:
+        print("L'huile ",TypeHuile," n'est pas reconnue.")
 
     demande=str(input("Tapez 'fini' si vous ne souhaitez pas ajouter d'autre huile à votre savon. "))
     if demande == "fini":
         continuer = False
-
-QSoudeTheorique=QSoudeKarite+QSoudeAmande+QSoudeCoco+QSoudeOlive
-print("La masse de soude pour saponifier totalement vos huiles est de ",QSoudeTheorique," g.")
+        print("La masse de soude pour saponifier totalement vos ",masse_totale_huiles," grammes d'huiles est de ",soude_totale_theorique," g.")
+indice_sapo_melange = indice_sapo_melange / masse_totale_huiles
 
 continuer=True #Demande de la masse de soude à utiliser pour la saponification
 while continuer :
@@ -84,9 +63,9 @@ while continuer :
             print("La quantité de soude ne peut pas être négative")
             MasseSoude=float(input("Entrer la quantité prévue de soude à utiliser (en g): "))
 
-    Taux=TauxSurgraissage (MasseSoude, MasseCoco, MasseKarite, MasseOlive, MasseAmande)
+    Taux=TauxSurgraissage (MasseSoude, masse_totale_huiles, indice_sapo_melange)
     print("Pour une masse de ",MasseSoude," g de soude, le taux de surgraissage du savon sera de ",Taux," %.")
-    TypeSavon=AfficheTypeSavon(Taux)
+    AfficheTypeSavon(Taux)
 
     demande=int(input("Tapez 0 si vous voulez quitter ce calcul ou n'importe quel autre chiffre si vous souhaitez continuer."))
     if demande == 0:
@@ -96,12 +75,12 @@ SoudePesee=float(input("Renseigner la masse de soude utilisée (en g) :")) #Calc
 while SoudePesee<0:
     print("La quantité de soude pesée ne peut pas être négative")
     SoudePesee=float(input("Renseigner la masse de soude utilisée (en g): "))
-Taux=TauxSurgraissage (SoudePesee, MasseCoco, MasseKarite, MasseOlive, MasseAmande)
+Taux=TauxSurgraissage (SoudePesee, masse_totale_huiles, indice_sapo_melange)
 print("Le taux de surgraissage calculé est de ",Taux,"%.")
+AfficheTypeSavon(Taux)
 
-TypeSavon=AfficheTypeSavon(Taux)
-
-MasseEau=float(input("Entrer la masse d'eau pesée (en g):")) #autres MP
+#autres MP
+MasseEau=float(input("Entrer la masse d'eau pesée (en g):"))
 while MasseEau<0:
     print("La quantité d'eau ne peut pas être négative")
     MasseEau=float(input("Entrer la masse d'eau pesée (en g):"))
@@ -116,13 +95,12 @@ while MasseColorant<0:
     print("La quantité de colorant ne peut pas être négative")
     MasseColorant=float(input("Entrer la masse de colorant pesée (en g):"))
 
-#Affichage graphique
-plt.figure(figsize = (10, 5))
-x = [MasseEau, MasseParfum, MasseColorant, MasseCoco, MasseKarite, MasseOlive, MasseAmande, SoudePesee]
+plt.figure(figsize = (10, 5)) #Affichage graphique
+x = [MasseEau, MasseParfum, MasseColorant, masse_totale_huiles, SoudePesee]
 normalize=True
-plt.pie(x, labels = ["Eau", "Parfum", "Colorant", "Coco", "Karité", "Olive", "Amande", "Soude"],
-           colors = ['blue', 'green', 'yellow','brown','brown','brown','brown','red'],
-           explode = [0, 0, 0, 0, 0, 0, 0, 0],
+plt.pie(x, labels = ["Eau", "Parfum", "Colorant", "Matières grasses", "Soude"],
+           colors = ['blue', 'green', 'yellow','brown','red'],
+           explode = [0, 0, 0, 0, 0],
            autopct = lambda x: str(round(x, 1)) + "%",
            pctdistance = 0.4, labeldistance = 0.6,
            shadow = True)
